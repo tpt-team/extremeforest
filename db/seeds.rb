b6c07deb3973592c863b860e6cf0d0cebea4bfdb
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2016-04-24 14:05:41 +0300

    add fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+titles = [
+  'Ножи',
+  'Палатки',
+  'Коврики',
+  'Веревки',
+  'Обувь'
+]
+
+titles.each { |i| Category.create(title: i) }
+
+#   Products should have name, description, image, price, category_id
+categories_ids = Category.pluck(:id)
+15.times do
+  Product.create!(name:       Faker::Commerce.product_name,
+                 description: Faker::Lorem.paragraph,
+                 photo:       Faker::Avatar.image,
+                 price:       Faker::Commerce.price,
+                 category_id: categories_ids.sample
+                 )
+end
