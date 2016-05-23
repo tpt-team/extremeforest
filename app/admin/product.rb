426ffa8469eeee9bc519fed062b9c6d211bba421
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2016-05-23 15:28:43 +0300

    Fix

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -9,7 +9,7 @@ ActiveAdmin.register Product do
     f.input :price
     f.input :category_id, :as => :select, :collection => Category.all.map {|u| [u.title.to_s, u.id]}
     f.input :subcategory_id, :as => :select, :collection => Subcategory.all.map {|u| [u.title.to_s, u.id]}
-    f.input :photo, :as => :file, :hint => f.template.image_tag(f.object.photo.url(:medium))
+    f.input :photo, :as => :file, :hint => f.template.image_tag(f.object.photo.url(download: true))
   end
   f.actions
 end
